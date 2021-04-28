e6cd45d412d2fec4850677fd67cd18c80fb4c1ab
Ezequiel T. Cruz <ezequielteixeiracr@gmail.com>
2021-04-28 17:56:57 -0300

    Criação clesse conta

---
+++ digioneback/src/main/kotlin/br/com/digioneback/Conta.kt
@@ -0,0 +1,17 @@
+package br.com.digioneback
+
+import java.math.BigDecimal
+
+class Conta (
+    var agencia: String,
+    var numero: String,
+    var saldo: BigDecimal
+){
+    fun deposito(valor: BigDecimal) {
+
+    }
+
+    fun saque(valor: BigDecimal) {
+
+    }
+}
